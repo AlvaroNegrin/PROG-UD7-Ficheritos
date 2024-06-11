b7bd171a38f3d1fbe5a2ef1294f360170bec78d0
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-06-11 13:36:59 +0200

    refact:Change methods name and logic to be more readable

--- src/net/salesianos/activity4/App.java
+++ src/net/salesianos/activity4/App.java
@@ -6,7 +6,8 @@ public class App {
     public static void main(String[] args) {
         Menu.setKeepAsking(true);
         while (Menu.isKeepAsking()) {
-            Menu.showMenu();
+            Menu.showMainMenu();
+            Menu.userSelection();
         }
         
     }
